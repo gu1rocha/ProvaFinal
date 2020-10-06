33f56db98b4cbeae36e551cdbf12ceb00e3e3770
gu1rocha <59288859+gu1rocha@users.noreply.github.com>
2020-10-06 11:55:41 -0300

    Update ProvaFinal.sql

--- ProvaFinal.sql
+++ ProvaFinal.sql
@@ -5,13 +5,13 @@ CREATE DATABASE empresa;
 USE empresa;
 CREATE TABLE departamento(
     codigo INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(50) NOT NULL,
+    nome VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE funcionario(
     codigo INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(50) NOT NULL,
-    salario VARCHAR(50) NOT NULL,
+    salario FLOAT(50) NOT NULL,
     codigo_departamento INT NOT NULL,
     CONSTRAINT FK_departamento FOREIGN KEY (codigo_departamento) REFERENCES departamento (codigo)
 );
